9fec767c50627eb4c48371a72b3c3af9a5f14cc9
nhathoangtran <nhathoangtran@gmail.com>
2013-05-08 21:29:12 +0700

    Initial style sheet added

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,8 +6,9 @@
     value="<?php echo set_value('username'); ?>"/>
   <br/>
   <label for="password">Password</label>
-  <input type="password" size="20" id="passowrd" name="password"/>
+  <input type="password" size="20" id="password" name="password"/>
   <br/>
   <input type="submit" value="Login"/>
 </form>
 <?php echo anchor('signup', 'Sign up', array('title' => 'Sign up for an account')); ?>
+
