ed495671d325e2f1c280f5698fb0bcb52f9b24aa
nhathoangtran <nhathoangtran@gmail.com>
2013-05-09 05:14:31 +0700

    Temporary view for Discard App (Applicant)

--- application/views/applicant/discard_app_view.php
+++ application/views/applicant/discard_app_view.php
@@ -3,4 +3,5 @@
   echo '<p>Are you sure to discard the application for the job "'.$app['Job_Name'].'" of employer "'.$app['Employer_Name'].'" with the CV "'.$app['Subject'].'" ?</p>';
 
   echo anchor('applicant/applications/discard_confirmed/'.$app['CID'].'/'.$app['JID'], 'Discard');
+  echo "&nbsp&nbsp&nbsp&nbsp&nbsp";
   echo anchor('applicant/applications', 'Cancel');
