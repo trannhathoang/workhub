0a44f29dc1cf8859aeacd6c2b6340ac4a0224036
nhathoangtran <nhathoangtran@gmail.com>
2013-05-09 11:56:03 +0700

    View CV view for Emp

--- application/views/employer/cv_view.php
+++ application/views/employer/cv_view.php
@@ -1,42 +1,57 @@
-<h2>View CV</h2>
-
+<h1>View CV</h1>
+<br />
 <p><?php if ($cv == NULL || $applicant == NULL) echo "Unknown CV"; else { ?></p>
 
-<h3>Applicant information</h3>
-<p>Name: <?php echo $applicant['Name']; ?></p>
-<p>Sex: <?php echo ($applicant['Sex'] == MALE ? 'Male' : 'Female'); ?></p>
-<p>Birthday: <?php echo $applicant['Birthday']; ?></p>
-<p>Email: <?php echo $applicant['Email']; ?></p>
-<p>Region: <?php
-  foreach ($regions as $row) {
-    if ($row['RID'] == $applicant['RID']) {
-      echo $row['Name'];
-      break;
-    }
-  }
-  ?>
-</p>
-<p>Address: <?php echo $applicant['Address']; ?></p>
-<p>Description: <?php echo $applicant['Description']; ?></p>
+<table border="0" style="text-align: left">
+	<tr>
+		<th style="padding-right: 30px; vertical-align: top;" rowspan="7">Applicant information</th>
+		<td>Name: <?php echo $applicant['Name']; ?></td>
+	</tr>
+	<tr>
+		<td>Sex: <?php echo ($applicant['Sex'] == MALE ? 'Male' : 'Female'); ?></td>
+	</tr>
+	<tr>
+		<td>Birthday: <?php echo $applicant['Birthday']; ?></td>
+	</tr>
+	<tr>
+		<td>Email: <?php echo $applicant['Email']; ?></td>
+	</tr>
+	<tr>
+		<td>
+			Region: <?php
+			  foreach ($regions as $row) {
+			    if ($row['RID'] == $applicant['RID']) {
+			      echo $row['Name'];
+			      break;
+			    }
+			  }
+			  ?>
+		</td>
+	</tr>
+	<tr><td>Address: <?php echo $applicant['Address']; ?></td></tr>
+	<tr><td>Description: <?php echo $applicant['Description']; ?></td></tr>
+	<tr><td> </td></tr>
+	<tr>
+		<th style="padding-right: 30px; vertical-align: top;" rowspan="7">Working information</th>
+		<td>CV's ID: <?php echo $cv['CID']; ?></td>
+	</tr>
+	<tr><td>Skills: <?php echo $cv['Skill']; ?></td></tr>
+	<tr><td>Education Level: <?php echo $cv['EduLev']; ?></td></tr>
+	<tr><td>Experience: <?php echo $cv['Exp']; ?></td></tr>
+	<tr><td>Languages: <?php echo $cv['Language']; ?></td></tr>
+	<tr><td>Additional Information: <?php echo $cv['AddInfo']; ?></td></tr>
+	<tr><td>Work region: <?php
+	  foreach ($regions as $row) {
+	    if ($row['RID'] == $cv['RID']) {
+	      echo $row['Name'];
+	      break;
+	    }
+	  }
+  ?></td></tr>
+  <tr><td></td></tr>
+</table>
 
-<h3>Working information</h3>
-<p>CV's ID: <?php echo $cv['CID']; ?></p>
-<p>Education Level: <?php echo $cv['EduLev']; ?></p>
-<p>Skills: <?php echo $cv['Skill']; ?></p>
-<p>Languages: <?php echo $cv['Language']; ?></p>
-<p>Experience: <?php echo $cv['Exp']; ?></p>
-<p>Additional Information: <?php echo $cv['AddInfo']; ?></p>
-<p>Work region: <?php
-  foreach ($regions as $row) {
-    if ($row['RID'] == $cv['RID']) {
-      echo $row['Name'];
-      break;
-    }
-  }
-  ?>
-</p
-
-<div>
+<div style="padding-bottom: 50px">
   <?php echo anchor('employer/cv/invite/'.$cv['CID'], 'Invite'); ?>
 </div>
 
