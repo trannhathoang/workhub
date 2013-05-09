da4a0e110b1aec28891deeb1284dd9dce36c9d10
nhathoangtran <nhathoangtran@gmail.com>
2013-05-09 12:48:34 +0700

    Profile view for Emp

--- application/views/employer/profile_view.php
+++ application/views/employer/profile_view.php
@@ -1,64 +1,65 @@
-<h2>Profile</h2>
+<h1>Profile</h1>
 
 <?php
 if (isset($updated) && $updated == TRUE) echo "<p>Profile is updated</p>";
 echo validation_errors();
 ?>
 
+<table style="text-align: left; vertical-align: top; padding-bottom: 50px" cellpadding="10" border="0">
 <?php echo form_open('employer/verify_profile'); ?>
-
-  <label for="type">Account type</label>
-  <input type="text" size="20" name="type" readonly
-    value="Employer"/>
-  <br/>
-
-  <label for="username">Username</label>
-  <input type="text" size="20" name="username" readonly
-    value="<?php echo $user['Username'];?>"/>
-  <br/>
-
-  <label for="password">Password</label>
-  <input type="password" size="20" name="password"/>
-  <br/>
-
-  <label for="confirm">Confirm password</label>
-  <input type="password" size="20" name="confirm"/>
-  <br/>
-
-  <label for="email">Email</label>
-  <input type="text" size="20" name="email"
-    value="<?php echo $user['Email']; ?>"/>
-  <br/>
-
-  <label for="name">Name</label>
-  <input type="text" size="20" name="name"
-    value="<?php echo $user['Name']; ?>"/>
-  <br/>
-
-  <label for="region">Region</label>
-  <select name="region">
+<tr>
+  <td><label for="type">Account type</label></td>
+  <td><input type="text" size="20" name="type" readonly
+    value="Employer"/></td>
+</tr>
+<tr>
+  <td><label for="username">Username</label></td>
+  <td><input type="text" size="20" name="username" readonly
+    value="<?php echo $user['Username'];?>"/></td>
+</tr>
+<tr>
+  <td><label for="password">Password</label></td>
+  <td><input type="password" size="20" name="password"/></td>
+</tr>
+<tr>
+  <td><label for="confirm">Confirm password</label></td>
+  <td><input type="password" size="20" name="confirm"/></td>
+</tr>
+<tr>
+  <td><label for="email">Email</label></td>
+  <td><input type="text" size="20" name="email"
+    value="<?php echo $user['Email']; ?>"/></td>
+</tr>
+<tr>
+  <td><label for="name">Name</label></td>
+  <td><input type="text" size="20" name="name"
+    value="<?php echo $user['Name']; ?>"/></td>
+</tr>
+<tr>
+  <td><label for="region">Region</label></td>
+  <td><select name="region">
     <?php
     foreach ($regions as $row) {
       echo '<option value="'.$row['RID'].'" '.($row['RID'] == $user['RID'] ? 'selected' : '').'>'.$row['Name'].'</option>';
     }
     ?>
-  </select>
-  <br/>
+  </select></td>
+</tr>
+<tr>
+  <td><label for="address">Address</label></td>
+  <td><input type="text" size="40" name="address"
+    value="<?php echo $user['Address']; ?>"/></td>
+</tr>
+<tr>
+  <td><label for="size">Size</label></td>
+  <td><input type="text" size="20" name="size"
+    value="<?php echo $user['Size']; ?>"/></td>
+</tr>
+<tr>
+  <td><label for="description">Description</label></td>
+  <td><textarea rows="5" cols="60" name="description"><?php echo $user['Description']; ?></textarea></td>
+</tr>
 
-  <label for="address">Address</label>
-  <input type="text" size="40" name="address"
-    value="<?php echo $user['Address']; ?>"/>
-  <br/>
-
-  <label for="size">Size</label>
-  <input type="text" size="20" name="size"
-    value="<?php echo $user['Size']; ?>"/>
-  <br/>
-
-  <label for="description">Description</label>
-  <textarea rows="5" cols="60" name="description"><?php echo $user['Description']; ?></textarea>
-  <br/>
-
-  <input type="submit" value="Update"/>
-
+  <tr><td colspan="2" style="text-align: center"><input type="submit" value="Update"/></td></tr>
 </form>
+</table>
