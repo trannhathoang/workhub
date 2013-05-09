2373520cdbef195ef6e5b7501eed36ca4396ad83
nhathoangtran <nhathoangtran@gmail.com>
2013-05-09 10:18:46 +0700

    Signup view and Navigation Bar - Employer

--- application/views/employer/nav_view.php
+++ application/views/employer/nav_view.php
@@ -1,7 +1,16 @@
-<ul>
-  <li><?php echo anchor('home', 'Home');?></li>
-  <li><?php echo anchor('employer/managejobs', 'Manage Jobs');?></li>
-  <li><?php echo anchor('employer/search', 'Search Applicants');?></li>
-  <li><?php echo anchor('employer/invitations', 'Invitations');?></li>
-  <li><?php echo anchor('home/profile', 'Profile');?></li>
-</ul>
+<div style="width:100%; float:left">
+	<div class="toplogo">
+		<img src="<?php echo base_url(); ?>/assets/images/me-employer-logo.jpg">
+	</div>
+	<div class="topbuttongroup">
+	  <?php echo anchor('home', 'Home');?> &nbsp;&nbsp;&nbsp;&nbsp;
+	  <?php echo anchor('employer/managejobs', 'Manage Jobs');?> &nbsp;&nbsp;&nbsp;&nbsp;
+	  <?php echo anchor('employer/search', 'Search Applicants');?> &nbsp;&nbsp;&nbsp;&nbsp;
+	  <?php echo anchor('employer/invitations', 'Invitations');?> &nbsp;&nbsp;&nbsp;&nbsp;
+	  <?php echo anchor('home/profile', 'Profile');?> &nbsp;&nbsp;
+	</div>
+</div>
+
+<div class="hor-sep">
+	<hr style="color: #333; width: 100%"/>
+</div>
--- application/views/employer/signup_view.php
+++ application/views/employer/signup_view.php
@@ -1,54 +1,55 @@
-<h1>Enter information</h1>
-
+<h1>Hello Employer!</h1>
+<h2>Please enter your information</h2>
 <?php echo validation_errors(); ?>
 
+<table style="text-align: left; padding-bottom: 50px" cellpadding="10" border="0">
 <?php echo form_open('employer/verify_signup'); ?>
-  <label for="username">Username</label>
-  <input type="text" size="20" id="username" name="username"
-    value="<?php echo set_value('username'); ?>"/>
-  <br/>
-
-  <label for="password">Password</label>
-  <input type="password" size="20" id="password" name="password"/>
-  <br/>
-
-  <label for="confirm">Confirm password</label>
-  <input type="password" size="20" id="confirm" name="confirm"/>
-  <br/>
-
-  <label for="email">Email</label>
-  <input type="text" size="20" id="email" name="email"
-    value="<?php echo set_value('email'); ?>"/>
-  <br/>
-
-  <label for="name">Name</label>
-  <input type="text" size="20" id="name" name="name"
-    value="<?php echo set_value('name'); ?>"/>
-  <br/>
-
-  <label for="region">Region</label>
-  <select name="region" value="24">
+<tr>
+  <td><label for="username">Username</label></td>
+  <td><input type="text" size="20" id="username" name="username"
+    value="<?php echo set_value('username'); ?>"/></td>
+</tr>
+<tr>
+  <td><label for="password">Password</label></td>
+  <td><input type="password" size="20" id="password" name="password"/></td>
+</tr>
+<tr>
+  <td><label for="confirm">Confirm password</label></td>
+  <td><input type="password" size="20" id="confirm" name="confirm"/></td>
+</tr>
+<tr>
+  <td><label for="email">Email</label></td>
+  <td><input type="text" size="20" id="email" name="email"
+    value="<?php echo set_value('email'); ?>"/></td>
+</tr>
+<tr>
+  <td><label for="name">Name</label></td>
+  <td><input type="text" size="20" id="name" name="name"
+    value="<?php echo set_value('name'); ?>"/></td>
+</tr>
+<tr>
+  <td><label for="region">Region</label></td>
+  <td><select name="region" value="24">
     <?php
     foreach ($regions as $row) {
       echo '<option value="'.$row['RID'].'">'.$row['Name'].'</option>';
     }
     ?>
-  </select>
-  <br/>
-
-  <label for="address">Address</label>
-  <input type="text" size="40" id="address" name="address"
-    value="<?php echo set_value('address'); ?>"/>
-  <br/>
-
-  <label for="size">Size</label>
-  <input type="text" size="20" id="size" name="size"
-    value="<?php echo set_value('size'); ?>"/>
-  <br/>
-
-  <label for="description">Description</label>
-  <textarea rows="5" cols="60" id="description" name="description"><?php echo set_value('description'); ?></textarea>
-  <br/>
-
-  <input type="submit" value="Sign up"/>
+  </select></td>
+</tr>
+<tr>
+  <td><label for="address">Address</label></td>
+  <td><input type="text" size="40" id="address" name="address"
+    value="<?php echo set_value('address'); ?>"/></td>
+</tr>
+<tr>
+  <td><label for="size">Size</label></td>
+  <td><input type="text" size="20" id="size" name="size"
+    value="<?php echo set_value('size'); ?>"/></td>
+</tr>
+<tr>
+  <td><label for="description">Description</label></td>
+  <td><textarea rows="5" cols="60" id="description" name="description"><?php echo set_value('description'); ?></textarea></td>
+</tr>
+<tr><td style="text-align: center" colspan="2"><input type="submit" value="Sign up"/></td></tr>
 </form>
